0eea55dddf6a37189f2480642b120ea575a42beb
MMRNB0038\techfunmmr <ycc.myatthwinmaung@gmail.com>
2018-10-07 15:53:15 +0630

    repair minimun function

--- Lecture1/src/Main2.java
+++ Lecture1/src/Main2.java
@@ -8,10 +8,10 @@ public class Main2 {
 	}
 
 	private static int searchMinimun(int... list) {
-		int min = 0;
-		for (int num : list) {
-			if (min > num)
-				min = num;
+		int min = list[0];
+		for (int i = 1; i < list.length; i++) {
+			if (min > list[i])
+				min = list[i];
 		}
 		return min;
 	}
