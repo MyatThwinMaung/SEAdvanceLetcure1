72865dc2d34c9e8c9cbfcc53785c38ecec47472c
MMRNB0038\techfunmmr <ycc.myatthwinmaung@gmail.com>
2018-10-07 15:39:34 +0630

    added lecture1 for searching maximun number

---
+++ Lecture1/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int maximumNum = calculateMaximum(5, 8, 1, 10, 52, 20, 2, 7, 6, 7);
+		System.out.println("Maximum number is " + maximumNum);
+	}
+
+	public static int calculateMaximum(int... list) {
+		int max = 0;
+		for (int num : list) {
+			if (max < num)
+				max = num;
+		}
+		return max;
+	}
+}
